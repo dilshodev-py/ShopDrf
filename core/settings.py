983b3d9bf1202463b2193cd3e17dd97adbdcf56c
dilshodev3234 <absaitovdev@gmail.com>
2024-09-13 17:09:02 +0500

    change to p23 name

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ REST_FRAMEWORK = {
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Your Project API',
-    'DESCRIPTION': 'Your project description',
+    'DESCRIPTION': 'P23 group description',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
     'COMPONENT_SPLIT_REQUEST': True,
