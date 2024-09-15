c45fb1649a288636a787f396d260f72f2cf05139
dilshodev3234 <absaitovdev@gmail.com>
2024-09-15 17:56:54 +0500

    p23

--- core/settings.py
+++ core/settings.py
@@ -118,6 +118,7 @@ MEDIA_ROOT = join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
