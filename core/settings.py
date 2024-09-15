d1e941982c8263f1d0825f48caee272963f6b89c
dilshodev3234 <absaitovdev@gmail.com>
2024-09-15 18:04:19 +0500

    p23

--- core/settings.py
+++ core/settings.py
@@ -126,8 +126,6 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
-
-
 SPECTACULAR_SETTINGS = {
     'TITLE': 'P23 group API',
     'DESCRIPTION': 'P23 group description',
